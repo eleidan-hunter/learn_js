f1c6bbcd678a3709a0b5e35120ce828f84920a25
eleidan-hunter <48258074+eleidan-hunter@users.noreply.github.com>
2021-05-13 16:21:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,111 +1,27 @@
-// const num = 0;
-// const nan = Nan;
+let num_1 = + prompt();
+let num_2 = + prompt();
+let min = num_1;
+let max = num_2;
 
-// const str = 'any hello';
+// verification min/max value
+if (num_1 < num_2){
+	min = num_1;
+	max = num_2;
+} else {
+	min = num_2;
+	max = num_1;
+}
 
-// const bool = true;
+console.log('min:', min);
+console.log('max:', max);
 
-// const object = {};
-// const array = [];
+// end verification min/max value
 
-// const _null = null;
-// const n = undefined; //mistake of js
+// random number
+let range = min + Math.random() * (max - min);
+console.log('range: ', range);
 
-// let a = 1;
-// let b = 2;
+let res = Math.round(range);
+console.log('res: ', res);
 
-// let c = a;
-// a = b;
-// b = c;
-
-// a = a + b; // a = 3
-// b = a - b; //
-// a = a - b; //
-
-// console.log('a = :', a);
-// console.log('b = :', b);
-
-//JSON.stringify => JSON.parse
-
-//==========================
-// bankomat
-
-// let enter = prompt();
-
-// console.log('enter:', enter);
-
-// let fifth = enter / 50;
-// fifth_q = parseInt(fifth);
-// fifthSum = fifth_q * 50;
-// res_1 = enter - fifthSum;
-
-// let twenty = res_1 / 20;
-// twenty_q = parseInt(twenty);
-// twentySum = twenty_q * 20;
-// res_2 = res_1 - twentySum;
-
-// let ten = res_2 / 10;
-// ten_q = parseInt(ten);
-// tenSum = ten_q * 10;
-// res_3 = res_2 - tenSum;
-
-// let rest = enter - (fifthSum + twentySum + tenSum);
-
-// console.log('50: ', fifth_q, '20: ', twenty_q, '10 :', ten_q, 'rest: ', rest);
-
-// =======================
-// // random numbers generator
-
-// let num_1 = + prompt();
-// let num_2 = + prompt();
-// let min = num_1;
-// let max = num_2;
-
-// // verification min/max value
-// if (num_1 < num_2){
-// 	min = num_1;
-// 	max = num_2;
-// } else {
-// 	min = num_2;
-// 	max = num_1;
-// }
-
-// console.log('min:', min);
-// console.log('max:', max);
-
-// // end verification min/max value
-
-// // random number
-// let range = min + Math.random() * (max - min);
-// console.log('range: ', range);
-
-// let res = Math.round(range);
-// console.log('res: ', res);
-
-// // end random number
-
-// ========================
-// true - false
-
-// let range = 1 + Math.random() * (9 - 1);
-// let num = Math.round(range);
-// console.log('number: ', num);
-
-// let userNumber = prompt('Enter your number between 1 and 9:');
-
-// if (userNumber >=1 && userNumber <= 9 ){
-//     if (userNumber === num) {
-//         alert('You win!');
-//     } else {
-//         alert('You lose :(');
-//     }
-// } else {
-//     alert('You enter wrong number!');
-// }
-
-// end true - false
-
-// for (let i = 10; -- i;){
-//     console.log(i); //9 8 7 6 5 4 3 2 2 1
-// }
-
+// enn random number
