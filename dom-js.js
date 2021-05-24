2d2154b8508f9ead28e13f248278382978e9edd4
eleidan-hunter <48258074+eleidan-hunter@users.noreply.github.com>
2021-05-24 21:36:04 +0300

    Add files via upload

---
+++ dom-js.js
@@ -0,0 +1,33 @@
+function addNewDeal(e){
+    e.preventDefault();
+    
+    if(input.value){
+        //first
+        // let count = document.querySelector('.listTitle span');
+        // count.innerHTML ++;
+
+        //second
+        let title = document.querySelector('.listTitle');
+        title.innerHTML = `To do list (${++count})`;
+        deals.push(input.value);
+        input.value = '';
+
+        console.log('deals', deals);
+    }
+
+    showDeals(deals, document.querySelector('.wrapper'));
+}
+let myButton = document.querySelector('#myButton');
+let input = document.querySelector('form input')
+let deals = [];
+let count = deals.length;
+
+myButton.onclick = addNewDeal;
+
+function showDeals(array, domElement){
+    // console.log('dom',domElement )
+    // domElement.innerHTML = array;
+    for (i = 0; i < array.length; i++){
+        domElement.innerHTML = array;
+    }
+}
