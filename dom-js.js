592e5201aff50069ff7a4c6c92f59b07bf34923d
eleidan-hunter <48258074+eleidan-hunter@users.noreply.github.com>
2021-05-27 21:32:32 +0300

    Add files via upload

--- dom-js.js
+++ dom-js.js
@@ -1,36 +1,62 @@
-function addNewDeal(e){
-    e.preventDefault();
-    
-    if(input.value){
-        //first
-        // let count = document.querySelector('.listTitle span');
-        // count.innerHTML ++;
-
-        //second
-        let title = document.querySelector('.listTitle');
-        title.innerHTML = `To do list (${++count})`;
-        deals.push(input.value);
-        input.value = '';
-
-        console.log('deals', deals);
-    }
-
-    showDeals(deals, document.querySelector('.wrapper'));
-}
+let title = document.querySelector('.listTitle');
 let myButton = document.querySelector('#myButton');
-let input = document.querySelector('form input')
+let input = document.querySelector('form input');
+let listDeals = document.querySelector('.wrapper');
 let deals = [];
 let count = deals.length;
 
-myButton.onclick = addNewDeal;
+// console.log(r4);
+myButton.addEventListener('click', addNewDeal);
+listDeals.addEventListener('click', removeDeals);
+
+function removeDeals(event){
+
+	event.preventDefault();
+	let elem = event.target;
+	// console.log('has',elem.hasAttribute('data-index'));
+	if(elem.hasAttribute('data-index')){
+		let attr = parseInt(elem.getAttribute('data-index'));
+		deals.splice(attr, 1);
+		showDeals(deals, listDeals);
+	}
+	// console.log('get',elem.getAttribute());
+	// console.log('-',elem.getAttribute());
+}
+
+function addNewDeal(e){
+	e.preventDefault();
+		
+	if(input.value){
+		// === first
+		// let count = document.querySelector('.listTitle span');
+		// count.innerHTML ++;
+
+		// === second
+		title.innerHTML = `To do list (${++count})`;
+		// deals.push(input.value);
+		// input.value = '';
+		deals.push({});
+		
+	}
+
+	showDeals(deals, listDeals);
+}
 
 function showDeals(array, domElement){
-    // console.log('dom',domElement )
-    // domElement.innerHTML = array;
-    for (i = 0; i < array.length; i++){
-        a = `<div><input type="checkbox"> ${array[i]} <button>Delete deal</button><div>`;
-        domElement.innerHTML = a
-        // sum += domElement.innerHTML;
-    }
-    
+	
+	domElement.innerHTML = array.reduce(
+		function(html, dealName, index){
+			html += `<div><input type="checkbox"> ${dealName} <button data-index='${index+1}'>Delete deal</button><div>`;
+			return html;
+		}, '');
+}
+
+function r4(){
+	let key = '';
+
+	for (i = 0; i < 4; i++){
+
+			key += Math.random().toString(36).replace('0.', '');
+	}
+	return key;
 }
---
+++ helper.js
@@ -0,0 +1,9 @@
+// function r4(){
+//     let key = '';
+
+//     for (i = 0; i < 4; i++){
+
+//         key += Math.random().toString(36).replace('0.', '');
+//     }
+//     return key;
+// }
