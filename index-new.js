a6d8ee973574f415e19492045f2161c59134a51f
eleidan-hunter <48258074+eleidan-hunter@users.noreply.github.com>
2021-06-04 17:15:48 +0300

    Add files via upload

--- index-new.js
+++ index-new.js
@@ -1,25 +1,57 @@
 let gallery = document.body;
-let Btn = document.querySelector('button');
+let BtnDefault = document.querySelector('.btn_20');
+let Btn100 = document.querySelector('.btn_100');
+let Btn200 = document.querySelector('.btn_200');
 
 
 
 // gallery.createElement('img');
 
-Btn.addEventListener('click', (e) => {
+// BtnDefault.addEventListener('click', (e) => {
+// 	document.querySelectorAll('img').forEach((e) => e.remove());
+// 	fetch(
+// 		'https://pixabay.com/api/?key=21923475-24100d30c0dcfaa14db447820&q=yellow+flowers&image_type=photo',
+// 		{
+// 			method: 'get',
+// 			headers: {},
+			
+// 		}
+// 	).then(( result ) => {
+// 		return result.json(); //text(), blob()
+// 	}).then((data) => {
+// 		data.hits.forEach(element => {
+// 			// console.log(element);
+// 			// first
+// 			// BtnDefault.insertAdjacentHTML('afterend', (`<img src="${element.previewURL}">`));
+
+// 			// second
+// 			let src = element.previewURL;
+// 			let Img = document.createElement('img');
+// 			Img.setAttribute( 'src', src);
+// 			document.body.appendChild(Img);
+// 			// console.log(Img);
+// 		});
+// 	});
+// });
+
+Btn100.addEventListener('click', (e) => {
 	document.querySelectorAll('img').forEach((e) => e.remove());
 	fetch(
-		'https://pixabay.com/api/?key=21923475-24100d30c0dcfaa14db447820&q=yellow+flowers&image_type=photo',
+		'https://pixabay.com/api/?key=21923475-24100d30c0dcfaa14db447820',
 		{
+			// key: '21923475-24100d30c0dcfaa14db447820',
 			method: 'get',
 			headers: {},
+			per_page: 100,
 		}
 	).then(( result ) => {
 		return result.json(); //text(), blob()
 	}).then((data) => {
+		console.log(data);
 		data.hits.forEach(element => {
 			// console.log(element);
 			// first
-			// Btn.insertAdjacentHTML('afterend', (`<img src="${element.previewURL}">`));
+			// Btn100.insertAdjacentHTML('afterend', (`<img src="${element.previewURL}">`));
 
 			// second
 			let src = element.previewURL;
@@ -30,3 +62,30 @@ Btn.addEventListener('click', (e) => {
 		});
 	});
 });
+
+// Btn200.addEventListener('click', (e) => {
+// 	document.querySelectorAll('img').forEach((e) => e.remove());
+// 	fetch(
+// 		'https://pixabay.com/api/?key=21923475-24100d30c0dcfaa14db447820&q=yellow+flowers&image_type=photo',
+// 		{
+// 			method: 'get',
+// 			headers: {},
+// 			per_page: 200,
+// 		}
+// 	).then(( result ) => {
+// 		return result.json(); //text(), blob()
+// 	}).then((data) => {
+// 		data.hits.forEach(element => {
+// 			// console.log(element);
+// 			// first
+// 			// Btn200.insertAdjacentHTML('afterend', (`<img src="${element.previewURL}">`));
+
+// 			// second
+// 			let src = element.previewURL;
+// 			let Img = document.createElement('img');
+// 			Img.setAttribute( 'src', src);
+// 			document.body.appendChild(Img);
+// 			// console.log(Img);
+// 		});
+// 	});
+// });
